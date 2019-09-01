9ad4bf3880592d3003e2cbd357af30528dde709c
Gunjan Khut <36451716+gkkhut@users.noreply.github.com>
2019-09-01 01:21:20 -0400

    1

---
+++ Logistic Regression/spyder_log_regression.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 19:10:01 2019
+
+@author: Sneha
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+import glob 
+from sklearn.model_selection import train_test_split
